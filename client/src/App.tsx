053eed2fd844074605dddb6663c9adefcd7b242e
Tim Tsai <sky22357168@gmail.com>
2021-06-02 18:07:53 +0800

    CRA Project Init

---
+++ client/src/App.tsx
@@ -0,0 +1,204 @@
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">Amazing Shop</a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./images/product-1.png" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Toy G</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+              </div>
+            </div>
+            <div className="price">
+              $ 120
+                </div>
+
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./images/product-1.png" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Toy G</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+              </div>
+            </div>
+            <div className="price">
+              $ 120
+                </div>
+
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./images/product-1.png" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Toy G</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+              </div>
+            </div>
+            <div className="price">
+              $ 120
+                </div>
+
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./images/product-1.png" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Toy G</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+              </div>
+            </div>
+            <div className="price">
+              $ 120
+                </div>
+
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./images/product-1.png" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Toy G</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+              </div>
+            </div>
+            <div className="price">
+              $ 120
+                </div>
+
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./images/product-1.png" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Toy G</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+              </div>
+            </div>
+            <div className="price">
+              $ 120
+                </div>
+
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./images/product-1.png" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Toy G</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star"></i>
+                </span>
+                <span><i className="fa fa-star-half"></i></span>
+                <span><i className="fa fa-star-o"></i></span>
+              </div>
+            </div>
+            <div className="price">
+              $ 120
+                </div>
+
+          </div>
+        </div>
+      </main>
+      <footer className="row center">
+        All right reserved
+    </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Mockdata.js
@@ -0,0 +1,70 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Top G1',
+            category: 'Gio',
+            image: '/images/p1.png',
+            price: 300,
+            brand: 'City',
+            rating: 4.5,
+            numReviews: 8,
+            description: 'blue robot'
+        },
+        {
+            _id: '2',
+            name: 'Top GP G2',
+            category: 'Gundam',
+            image: '/images/p2.png',
+            price: 280,
+            brand: 'City',
+            rating: 3,
+            numReviews: 3,
+            description: 'robot with orrnage'
+        },
+        {
+            _id: '3',
+            name: 'Guit G3',
+            category: 'Unioql',
+            image: '/images/p3.png',
+            price: 500,
+            brand: 'Bandi',
+            rating: 2.5,
+            numReviews: 7,
+            description: 'bGG suit'
+        },
+        {
+            _id: '4',
+            name: 'Top G4',
+            category: 'Gundam',
+            image: '/images/p4.png',
+            price: 1230,
+            brand: 'Ghio',
+            rating: 3,
+            numReviews: 4,
+            description: 'blue robot'
+        },
+        {
+            _id: '5',
+            name: 'TYGI',
+            category: 'Cioo',
+            image: '/images/p5.png',
+            price: 3000,
+            brand: 'Bandi',
+            rating: 4.5,
+            numReviews: 3,
+            description: 'blue robot'
+        },
+        {
+            _id: '6',
+            name: 'FAFA G',
+            category: 'Cioo',
+            image: '/images/p6.png',
+            price: 30,
+            brand: 'Jueq',
+            rating: 5,
+            numReviews: 3,
+            description: 'Hello FG'
+        },
+    ]
+}
