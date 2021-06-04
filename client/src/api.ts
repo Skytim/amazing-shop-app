671aadb40362e7544c801a1aad5aab0e835085c8
Tim Tsai <sky22357168@gmail.com>
2021-06-04 08:54:23 +0800

    Add api

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const request = axios.create({
-  baseURL: 'https://arcane-badlands-11766.herokuapp.com/'
+  baseURL: process.env.NODE_ENV === 'development' ? 
+  'http://localhost:5000/' :
+  'https://arcane-badlands-11766.herokuapp.com/'
 });
 
 export const productData = () => request.get('/api/products');
